9d130ffc94d29655e79e0b7a127e115682b8c75d
ZhuDailing <461323274@qq.com>
2017-12-04 18:15:35 -0500

    put all variables to site

--- src/project_adb/Site.java
+++ src/project_adb/Site.java
@@ -11,7 +11,22 @@ public class Site {
 		variable = new HashMap<>();
 		isUp = true;
 		this.siteIndex = index;
+		
 		// put all variables to this site along with exist
+		for (int i = 1; i <= 20; i++) {
+			Variable var = new Variable(i);
+			if (i % 2 == 0) {
+				variable.put(var, true);
+				//isReady.put(var.getVariableID(), true);
+			} else if ((i + 1) % 10 == siteIndex) {
+				variable.put(var, true);
+				//isReady.put(var.getVariableID(), true);
+			} else {
+				variable.put(var, false);
+				//isReady.put(var.getVariableID(), false);
+			}
+			//locktable.put(var, new HashMap<Transaction, Lock>());
+		}
 	}
 	
 	protected void failSite() {
